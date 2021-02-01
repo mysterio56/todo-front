c6a344a0918f6e802ac178321366540bba674abc
Francisco Sánchez <fjsanchezrosales@gmail.com>
2021-02-01 04:49:05 -0600

    remove handle

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <>
       <TableTasks />
-      <ModalAddTask handleClose={ () => { handleClose() } }/>
+      <ModalAddTask />
       <DrawerEditTask />
     </>
   );
